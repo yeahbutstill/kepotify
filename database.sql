2b0f374469dd01421c846c91642531a8bb615cdc
yeahbutstill <mayadani28@gmail.com>
2023-05-10 17:13:13 +0700

    fixing schema

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
-    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     email CHARACTER VARYING(150) NOT NULL,
     name CHARACTER VARYING(150) NOT NULL,
     birthday DATE NOT NULL,
@@ -14,36 +14,41 @@ CREATE TABLE users (
     status_record CHARACTER VARYING(150) NOT NULL
 );
 
-ALTER TABLE users ADD COLUMN created_by CHARACTER VARYING(150);
-ALTER TABLE users ADD COLUMN updated_by CHARACTER VARYING(150);
-ALTER TABLE users ADD COLUMN created_at TIMESTAMP;
-ALTER TABLE users ADD COLUMN updated_at TIMESTAMP;
-ALTER TABLE users ADD COLUMN environment CHARACTER VARYING(150);
-ALTER TABLE users ADD COLUMN status_record CHARACTER VARYING(150);
-
-CREATE TABLE follows (
+CREATE TABLE follow_artists (
     user_id UUID DEFAULT uuid_generate_v4() NOT NULL,
-    artist_id UUID DEFAULT uuid_generate_v4(),
-    playlist_id UUID DEFAULT uuid_generate_v4(),
-    CONSTRAINT fk_user_follows FOREIGN KEY (user_id) REFERENCES users (id),
-    CONSTRAINT fk_artist_follows FOREIGN KEY (artist_id) REFERENCES artists (id),
-    CONSTRAINT fk_playlist_follows FOREIGN KEY (playlist_id) REFERENCES playlists (id),
-    PRIMARY KEY (user_id, artist_id, playlist_id)
+    artist_id UUID NULL,
+    CONSTRAINT fk_user_follow_artists FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_artist_follow_artists FOREIGN KEY (artist_id) REFERENCES artists (id),
+    PRIMARY KEY (user_id, artist_id)
+);
+
+CREATE TABLE follow_playlists (
+    user_id UUID DEFAULT uuid_generate_v4() NOT NULL,
+    playlist_id UUID NULL,
+    CONSTRAINT fk_user_follow_playlists FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_playlist_follow_playlists FOREIGN KEY (playlist_id) REFERENCES playlists (id),
+    PRIMARY KEY (user_id, playlist_id)
 );
 
 CREATE TABLE artists (
-    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     name CHARACTER VARYING(150) NOT NULL,
     instagram CHARACTER VARYING(150),
     facebook CHARACTER VARYING(150),
     twitter CHARACTER VARYING(150),
     wikipedia CHARACTER VARYING(150),
-    bio TEXT
+    bio TEXT,
+    created_by CHARACTER VARYING(150),
+    updated_by CHARACTER VARYING(150),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    environment CHARACTER VARYING(150) NOT NULL,
+    status_record CHARACTER VARYING(150) NOT NULL
 );
 
 CREATE TABLE sings (
     artist_id UUID DEFAULT uuid_generate_v4() NOT NULL,
-    song_id UUID DEFAULT uuid_generate_v4() NOT NULL,
+    song_id UUID NOT NULL,
     CONSTRAINT fk_artist_sing FOREIGN KEY (artist_id) REFERENCES artists (id),
     CONSTRAINT fk_song_sing FOREIGN KEY (song_id) REFERENCES songs (id),
     PRIMARY KEY (artist_id, song_id)
@@ -51,24 +56,36 @@ CREATE TABLE sings (
 
 CREATE TABLE has_artist_albums (
     artist_id UUID DEFAULT uuid_generate_v4() NOT NULL,
-    album_id UUID DEFAULT uuid_generate_v4() NOT NULL,
+    album_id UUID NOT NULL,
     CONSTRAINT fk_artist_has FOREIGN KEY (artist_id) REFERENCES artists (id),
     CONSTRAINT fk_albums_has FOREIGN KEY (album_id) REFERENCES albums (id),
     PRIMARY KEY (artist_id, album_id)
 );
 
 CREATE TABLE albums (
-    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     title CHARACTER VARYING(150) NOT NULL,
     release CHARACTER VARYING(150) NOT NULL,
-    image CHARACTER VARYING(150) NOT NULL
+    image CHARACTER VARYING(150) NOT NULL,
+    created_by CHARACTER VARYING(150),
+    updated_by CHARACTER VARYING(150),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    environment CHARACTER VARYING(150) NOT NULL,
+    status_record CHARACTER VARYING(150) NOT NULL
 );
 
 CREATE TABLE songs (
-    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-    album_id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    album_id UUID NOT NULL,
     title CHARACTER VARYING(150) NOT NULL,
     duration INT NOT NULL,
+    created_by CHARACTER VARYING(150),
+    updated_by CHARACTER VARYING(150),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    environment CHARACTER VARYING(150) NOT NULL,
+    status_record CHARACTER VARYING(150) NOT NULL,
     CONSTRAINT fk_albums_songs FOREIGN KEY (album_id) REFERENCES albums(id)
 );
 
@@ -78,19 +95,31 @@ CREATE TABLE concerts (
     lon DOUBLE PRECISION,
     lat DOUBLE PRECISION,
     event_at TIMESTAMP NOT NULL ,
-    url CHARACTER VARYING(150)NOT NULL
+    url CHARACTER VARYING(150)NOT NULL,
+    created_by CHARACTER VARYING(150),
+    updated_by CHARACTER VARYING(150),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    environment CHARACTER VARYING(150) NOT NULL,
+    status_record CHARACTER VARYING(150) NOT NULL
 );
+ALTER TABLE concerts ADD COLUMN created_by CHARACTER VARYING(150);
+ALTER TABLE concerts ADD COLUMN updated_by CHARACTER VARYING(150);
+ALTER TABLE concerts ADD COLUMN created_at TIMESTAMP;
+ALTER TABLE concerts ADD COLUMN updated_at TIMESTAMP;
+ALTER TABLE concerts ADD COLUMN environment CHARACTER VARYING(150);
+ALTER TABLE concerts ADD COLUMN status_record CHARACTER VARYING(150);
 
 CREATE TABLE performs (
     artist_id UUID DEFAULT uuid_generate_v4() NOT NULL,
-    concert_id UUID DEFAULT uuid_generate_v4() NOT NULL,
+    concert_id UUID NOT NULL,
     CONSTRAINT fk_artist_performs FOREIGN KEY (artist_id) REFERENCES artists (id),
     CONSTRAINT fk_concert_performs FOREIGN KEY (concert_id) REFERENCES concerts (id),
     PRIMARY KEY (artist_id, concert_id)
 );
 
 CREATE TABLE playlist_categories (
-    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     name CHARACTER VARYING(150) NOT NULL,
     image CHARACTER VARYING(150) NOT NULL,
     icon CHARACTER VARYING(150) NOT NULL,
@@ -104,8 +133,8 @@ CREATE TABLE playlist_categories (
 
 CREATE TABLE playlists (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
-    user_id UUID DEFAULT uuid_generate_v4() NOT NULL,
-    playlist_categories_id UUID DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NULL,
+    playlist_categories_id UUID DEFAULT NULL,
     name CHARACTER VARYING(150) NOT NULL,
     description TEXT,
     image CHARACTER VARYING(150),
@@ -117,4 +146,20 @@ CREATE TABLE playlists (
     status_record CHARACTER VARYING(150) NOT NULL,
     CONSTRAINT fk_user_playlist FOREIGN KEY (user_id) REFERENCES users (id),
     CONSTRAINT fk_category_playlist FOREIGN KEY (playlist_categories_id) REFERENCES playlist_categories (id)
+);
+
+CREATE TABLE contain_playlists (
+    song_id UUID DEFAULT uuid_generate_v4() NOT NULL,
+    playlist_id UUID NOT NULL,
+    CONSTRAINT fk_song_contain_playlists FOREIGN KEY (song_id) REFERENCES songs (id),
+    CONSTRAINT fk_playlist_contain_playlist FOREIGN KEY (playlist_id) REFERENCES playlists (id),
+    PRIMARY KEY (song_id, playlist_id)
+);
+
+CREATE TABLE users_like_songs (
+    user_id UUID NOT NULL,
+    song_id UUID NOT NULL,
+    CONSTRAINT fk_users_users_like_songs FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_songs_users_like_songs FOREIGN KEY (song_id) REFERENCES songs (id),
+    PRIMARY KEY (user_id, song_id)
 );
