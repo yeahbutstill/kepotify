2df4939fdd7a9e066e2c6a517eb8fdd083b975b0
yeahbutstill <mayadani28@gmail.com>
2023-05-15 12:14:06 +0700

    test pessimistic locking

--- src/test/java/com/yeahbutstill/kepotify/LockingTest.java
+++ src/test/java/com/yeahbutstill/kepotify/LockingTest.java
@@ -7,6 +7,7 @@ import com.yeahbutstill.kepotify.utils.JpaUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.LockModeType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -82,6 +83,47 @@ class LockingTest {
 
         entityManager.persist(transactionCredit);
         Assertions.assertNotNull(transactionCredit.getVersion());
+
+        entityTransaction.commit();
+        entityManager.close();
+
+    }
+
+    @Test
+    void testUpdatePessimisticLocking1() throws InterruptedException {
+
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        TransactionCredit transactionCredit = entityManager
+                .find(TransactionCredit.class,
+                        UUID.fromString("e0c04ea6-e7b3-4009-8e37-3b3a7aa7c537"),
+                        LockModeType.PESSIMISTIC_WRITE);
+        transactionCredit.setBalance(new BigDecimal(50_000));
+        Thread.sleep(10 * 1000L);
+        entityManager.persist(transactionCredit);
+
+        entityTransaction.commit();
+        entityManager.close();
+
+    }
+
+    @Test
+    void testUpdatePessimisticLocking2() {
+
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        TransactionCredit transactionCredit = entityManager
+                .find(TransactionCredit.class,
+                        UUID.fromString("e0c04ea6-e7b3-4009-8e37-3b3a7aa7c537"),
+                        LockModeType.PESSIMISTIC_WRITE);
+        transactionCredit.setBalance(new BigDecimal(5_000_000));
+        entityManager.persist(transactionCredit);
 
         entityTransaction.commit();
         entityManager.close();
